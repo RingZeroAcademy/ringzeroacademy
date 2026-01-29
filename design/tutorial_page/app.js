b6e0fecb55d35c92660d28ec018eab6b39e08859
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-29 16:46:04 +0300

    Add siderail and open sidebar button.

--- design/tutorial_page/app.js
+++ design/tutorial_page/app.js
@@ -1,7 +1,14 @@
+const openSidebarButton = document.getElementById("openSidebarButton");
 const closeSidebarButton = document.getElementById("closeSidebarButton");
 const sidebar = document.getElementById("sidebar");
 const content = document.getElementById("content");
+
 closeSidebarButton.addEventListener("click", event => {
     sidebar.classList.add("hidden");
     content.classList.add("expanded");
+});
+
+openSidebarButton.addEventListener("click", event => {
+    sidebar.classList.remove("hidden");
+    content.classList.remove("expanded");
 });
