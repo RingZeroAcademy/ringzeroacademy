de11b134f069170c8f64beceafef610ba621246b
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-02-08 13:58:27 +0300

    Add functionality to the toggle sidebar button and remove the open and close sidebar buttons.

--- design/tutorial_page/app.js
+++ design/tutorial_page/app.js
@@ -1,14 +1,15 @@
-const openSidebarButton = document.getElementById("openSidebarButton");
-const closeSidebarButton = document.getElementById("closeSidebarButton");
+const toggleSidebarButton = document.getElementById("toggleSidebarButton");
 const sidebar = document.getElementById("sidebar");
 const content = document.getElementById("content");
 
-closeSidebarButton.addEventListener("click", event => {
-    sidebar.classList.add("hidden");
-    content.classList.add("expanded");
-});
-
-openSidebarButton.addEventListener("click", event => {
-    sidebar.classList.remove("hidden");
-    content.classList.remove("expanded");
+toggleSidebarButton.addEventListener("click", event => {
+    if (sidebar.classList.contains("hidden")) {
+        sidebar.classList.remove("hidden");
+        content.classList.remove("expanded");
+        toggleSidebarButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-close-icon lucide-panel-left-close"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/><path d="m16 15-3-3 3-3"/></svg>'
+    } else {
+        sidebar.classList.add("hidden");
+        content.classList.add("expanded");
+        toggleSidebarButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-open-icon lucide-panel-left-open"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/><path d="m14 9 3 3-3 3"/></svg>';
+    }
 });
