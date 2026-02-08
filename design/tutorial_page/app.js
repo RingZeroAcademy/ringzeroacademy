75c73b35d6d6a6c2fe78966c971838e7936682d1
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-02-08 16:24:38 +0300

    Make sidebar responsive and change sidebar toggle button icon.

--- design/tutorial_page/app.js
+++ design/tutorial_page/app.js
@@ -1,15 +1,31 @@
-const toggleSidebarButton = document.getElementById("toggleSidebarButton");
 const sidebar = document.getElementById("sidebar");
 const content = document.getElementById("content");
-
+const toggleSidebarButton = document.getElementById("toggleSidebarButton");
 toggleSidebarButton.addEventListener("click", event => {
-    if (sidebar.classList.contains("hidden")) {
-        sidebar.classList.remove("hidden");
-        content.classList.remove("expanded");
-        toggleSidebarButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-close-icon lucide-panel-left-close"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/><path d="m16 15-3-3 3-3"/></svg>'
-    } else {
-        sidebar.classList.add("hidden");
-        content.classList.add("expanded");
-        toggleSidebarButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-open-icon lucide-panel-left-open"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/><path d="m14 9 3 3-3 3"/></svg>';
-    }
+    sidebar.classList.toggle("display");
+    content.classList.toggle("shrink");
 });
+
+// Sidebar responsiveness.
+const windowSizeQuery = window.matchMedia("(min-width: 641px)");
+function toggleSidebar(event) {
+    /*
+    Hides or displays the sidebar based on the screen size.
+    The sidebar is hidden for screens less than 641px and displayed
+    for larger screens.
+
+    Param event:
+        A MediaQueryList or MediaQueryListEvent object returned from
+        a media query with this media query string "(min-width: 641px)".
+    */
+    if (event.matches) {
+        sidebar.classList.add("display");
+        content.classList.add("shrink");
+    } else {
+        sidebar.classList.remove("display");
+        content.classList.remove("shrink");
+    }
+}
+toggleSidebar(windowSizeQuery);
+windowSizeQuery.addEventListener("change", toggleSidebar);
+
